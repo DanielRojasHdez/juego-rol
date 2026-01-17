31301c91404b6a82a842ccde924e0b6d3a35ff26
Bernat <bernat_mp3@hotmail.com>
2026-01-17 14:32:15 +0000

    Commit Bernat (clases creadas)

--- personajes.py
+++ personajes.py
@@ -2,4 +2,15 @@
 # git commit -m "Commit Bernat (Breve explicación)"
 # git push origin rama-bernat
 
-print("Hola4")
+class Personaje:
+    def __init__(self, nombre, vida, vida_max, ataque_base, defendido, armadura):
+        self.nombre = nombre
+        self.vida = vida_max
+        self.vida_max = vida_max
+        self.ataque_base = ataque_base
+        self.defendido = defendido
+        self.armadura = armadura
+
+heroe = Personaje("Héroe", 100, 100, 10, False, False)
+soldado = Personaje("Soldado", 35, 35, 5, None, None)
+jefe = Personaje("Jefe", 50, 50, 7.5, None, None)
