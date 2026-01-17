977a60830e6e565fe8cad838a04a5c0ee3a24149
Bernat <bernat_mp3@hotmail.com>
2026-01-17 18:10:36 +0000

    Commit Bernat (Subida de algunas funciones)

--- personajes.py
+++ personajes.py
@@ -2,6 +2,11 @@
 # git commit -m "Commit Bernat (Breve explicación)"
 # git push origin rama-bernat
 
+
+import random
+
+# Clases
+
 class Personaje:
     def __init__(self, nombre, vida, vida_max, ataque_base, defendido, armadura):
         self.nombre = nombre
@@ -12,5 +17,47 @@ class Personaje:
         self.armadura = armadura
 
 heroe = Personaje("Héroe", 100, 100, 10, False, False)
-soldado = Personaje("Soldado", 35, 35, 5, None, None)
-jefe = Personaje("Jefe", 50, 50, 7.5, None, None)
+soldado = Personaje("Soldado", 35, 35, random.randint(4, 6), None, None)
+jefe = Personaje("Jefe", 50, 50, random.randint(7, 8), None, None)
+
+
+# print(soldado.ataque_base)
+# Funciones
+# Fuincion para incremento de nivel dependiendo oleada
+
+
+def increment_nivel (Personaje):
+    incremento = 2
+    Personaje.ataque_base += incremento
+
+# Incremento +2 puntos al ataque del soldado
+# print(jefe.ataque_base)
+# increment_nivel(jefe)
+# print(jefe.ataque_base)
+
+
+# Función de cambio de vida al ser atacado. Resta vida actual del heroe dependiendo del ataque del soldado o jefe
+
+
+def newhp (Personaje, daño):
+    Personaje.vida = Personaje.vida - daño
+
+    if Personaje.vida < 0:
+        Personaje.vida = 0
+
+# print (heroe.vida)
+# print (jefe.ataque_base)
+# newhp(heroe, jefe.ataque_base)
+# print(heroe.vida)
+# newhp(heroe, jefe.ataque_base)
+# print(heroe.vida)
+
+#Funcion para determinar si el personaje sigue vivo
+
+def sigue_vivo (Personaje):
+    if Personaje.vida > 0:
+        return True  
+    else:
+        return False
+    
+# print (sigue_vivo(heroe))
