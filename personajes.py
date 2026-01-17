1b33438b56f55dd0c3fbdb140d643f85b2223ba9
Bernat <bernat_mp3@hotmail.com>
2026-01-17 19:05:35 +0000

    Commit Bernat (Actualización de funciones)

--- personajes.py
+++ personajes.py
@@ -8,12 +8,12 @@ import random
 # Clases
 
 class Personaje:
-    def __init__(self, nombre, vida, vida_max, ataque_base, defendido, armadura):
+    def __init__(self, nombre, vida, vida_max, ataque_base, defenderse, armadura):
         self.nombre = nombre
-        self.vida = vida_max
+        self.vida = vida
         self.vida_max = vida_max
         self.ataque_base = ataque_base
-        self.defendido = defendido
+        self.defenderse = defenderse
         self.armadura = armadura
 
 heroe = Personaje("Héroe", 100, 100, 10, False, False)
@@ -26,9 +26,9 @@ jefe = Personaje("Jefe", 50, 50, random.randint(7, 8), None, None)
 # Fuincion para incremento de nivel dependiendo oleada
 
 
-def increment_nivel (Personaje):
+def increment_nivel (personaje):
     incremento = 2
-    Personaje.ataque_base += incremento
+    personaje.ataque_base += incremento
 
 # Incremento +2 puntos al ataque del soldado
 # print(jefe.ataque_base)
@@ -39,11 +39,11 @@ def increment_nivel (Personaje):
 # Función de cambio de vida al ser atacado. Resta vida actual del heroe dependiendo del ataque del soldado o jefe
 
 
-def newhp (Personaje, daño):
-    Personaje.vida = Personaje.vida - daño
+def newhp (personaje, daño):
+    personaje.vida = personaje.vida - daño
 
-    if Personaje.vida < 0:
-        Personaje.vida = 0
+    if personaje.vida < 0:
+        personaje.vida = 0
 
 # print (heroe.vida)
 # print (jefe.ataque_base)
@@ -54,10 +54,31 @@ def newhp (Personaje, daño):
 
 #Funcion para determinar si el personaje sigue vivo
 
-def sigue_vivo (Personaje):
-    if Personaje.vida > 0:
+def sigue_vivo (personaje):
+    if personaje.vida > 0:
         return True  
     else:
         return False
     
 # print (sigue_vivo(heroe))
+
+
+#Funcion para curar el personaje, por ejemplo con pocion o magia de cura
+def curar (personaje, cantidad):
+    personaje.vida += cantidad
+
+    if personaje.vida > personaje.vida_max:
+        personaje.vida = personaje.vida_max
+
+#Funcion para activar defensa y reducir un 20% daño recibido, se desactiva al usarse.
+def defensa_on (personaje, ataque_base):
+    if personaje.defenderse:
+        daño = ataque_base * 0.20
+        personaje.defenderse = False  # se desactiva tras usarla
+    else:
+        daño = ataque_base
+    
+    personaje.vida -= daño
+    
+    if personaje.vida < 0:
+        personaje.vida = 0
